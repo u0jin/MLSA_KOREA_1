08acd726983f99f29b6a1ba81e8b2a1f5032338c
ujinee <yujin5836@gmail.com>
2021-08-02 23:50:34 +0900

    api change

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,56 @@
+import React from "react";
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HackaLearn Korea_youjin
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  async getData() {
+    const res = await axios.get('http://localhost:7071/api/datetimes');
+    return res.data.message; 
+}
+    constructor(...args) {
+        super(...args);
+        this.state = {data: null};
+    }
+
+
+  componentDidMount() {
+    if (!this.state.data) {
+        (async () => {
+            try {
+               this.setState({data: await this.getData()});
+            } catch (e) {
+            }
+        })();
+    }
+
+  }
+
+
+  render() {
+    
+    return (
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            MLSA_HackaLearn
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Time : {this.state.data}
+          </a>
+        </header>
+  
+      </div>
+  
+    );
+
+  }
 }
 
-export default App;
+
